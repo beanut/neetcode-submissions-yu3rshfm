8a2689225d33392fa48bccfded815dcc8c72a50e
Bernard Lim <40220988+beanut@users.noreply.github.com>
2026-05-15 13:32:26 -0400

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+
+        res = []
+
+        comb = []
+        sum = 0
+
+        def dfs(start: int):
+            nonlocal sum
+            if sum >= target:
+                if sum == target:
+                    res.append(comb[:])
+                return
+            
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                comb.append(candidates[i])
+                sum += candidates[i]
+
+                dfs(i + 1)
+
+                sum -= candidates[i]
+                comb.pop()
+
+        dfs(0)
+        return res
