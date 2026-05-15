f86d521cff4d84e43ccfb65a3b6f8e260ea8628e
Bernard Lim <40220988+beanut@users.noreply.github.com>
2026-05-14 23:47:42 -0400

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        # we sort nums and enforce an order in recursion
+        def dfs(curMinIdx: int, sum: int, cur: List[int]):
+            if sum >= target:
+                if sum == target:
+                    res.append(cur)
+                return
+            
+            for i in range(curMinIdx, len(nums)):
+                dfs(max(curMinIdx, i), sum + nums[i], cur + [nums[i]])
+        
+        dfs(0, 0, [])
+        return res
