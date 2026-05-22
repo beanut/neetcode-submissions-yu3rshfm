45273c3c147383908388b18d68007262be603bab
Bernard Lim <40220988+beanut@users.noreply.github.com>
2026-05-21 21:25:59 -0400

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+
+        lmax = max(0, height[l])
+        rmax = max(0, height[r])
+
+        water = 0
+
+        while l < r:
+            if height[l] <= height[r]:
+                lmax = max(lmax, height[l])
+                water += lmax - height[l]
+                l += 1
+            else:
+                rmax = max(rmax, height[r])
+                water += rmax - height[r]
+                r -= 1
+                
+        return water
